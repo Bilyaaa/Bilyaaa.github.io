a525385555a5e940cb4bb53f30c79fc70e3b12d6
Павел Гуд <bilyaa0@gmail.com>
2022-10-13 17:28:00 +0300

    another try

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import  Burger  from "../components/Burger";
+import  $  from "jquery";
 import React from "react";
 import "../styles/Header.scss";
 import { Link, Outlet } from "react-router-dom";
 
 
 function Header() {
+
+  
+function Burger() {
+ 
+  function Menu() {
+      document.querySelector('.nav-small-opened').classList.remove('hidden')
+      $('.nav-small').css('display', 'none')
+      $('.nav-small-opened div').css('opacity', '1')
+  }
+
+  function hideMenu() {
+    $('.nav-small-opened').classList.add('hidden')
+    $('.nav-small').css('display', 'flex')
+  }
+
+  return (
+    <>
+    <div className="nav-small" onClick={Menu}>
+      <div>-</div>
+      <div>-</div>
+      <div>-</div>
+    </div>
+    <div className="nav-small-opened hidden">
+      <Link to="/shop" onClick={hideMenu}><div>SHOP</div></Link>
+      <Link to="/delivery" onClick={hideMenu}><div>DELIVERY</div></Link>
+      <Link to="/admin" onClick={hideMenu}><div>ADMIN</div></Link>
+    </div>
+    </>
+  );
+}
 
 
   return (
